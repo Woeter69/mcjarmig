864c90b18b76d8211102adb630bac6fea2d440b4
Woeter <woeter@local.dev>
2026-07-15 17:45:10 +0530

    feat(cli): implement basic command-line flag parsing using flag package

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
 type Config struct {
@@ -13,5 +15,22 @@ type Config struct {
 }
 
 func main() {
-	fmt.Println("ModMigrator initializing...")
+	cfg := parseFlags()
+	if cfg.Version == "" || cfg.Loader == "" {
+		fmt.Println("Error: both -version and -loader flags are required.")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	fmt.Printf("Starting ModMigrator for version %s with loader %s
+", cfg.Version, cfg.Loader)
+}
+
+func parseFlags() *Config {
+	cfg := &Config{}
+	flag.StringVar(&cfg.ModsDir, "dir", "./mods", "Path to the mods folder")
+	flag.StringVar(&cfg.Version, "version", "", "Target Minecraft version to migrate to [Required]")
+	flag.StringVar(&cfg.Loader, "loader", "", "Target mod loader [Required]")
+	flag.IntVar(&cfg.Workers, "workers", 5, "Number of concurrent workers")
+	flag.Parse()
+	return cfg
 }
